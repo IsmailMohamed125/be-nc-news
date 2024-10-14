d010f9b0518dd44692906de617418650825a9218
ismail125 <im.1369m@gmail.com>
2024-10-14 11:41:23 +0100

    Created conrollers, models, routes

---
+++ controllers/topics.controller.js
@@ -0,0 +1,13 @@
+const { selectTopics } = require("../models/topics");
+
+const getAllTopics = (req, res, next) => {
+  selectTopics()
+    .then((topics) => {
+      res.status(200).send({ topics });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+module.exports = { getAllTopics };
