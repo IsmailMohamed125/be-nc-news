00a664899eadd0b307fe5acf8b9984cdccd7d687
ismail125 <im.1369m@gmail.com>
2024-10-14 14:03:53 +0100

    Error handling middleware

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routes/apiRoutes");
-const topicsRouter = require("./routes/topicsRoutes");
-const articlesRouter = require("./routes/articlesRoutes");
 
 app.use("/api", apiRouter);
-app.use("/api/topics", topicsRouter);
-app.use("/api/articles", articlesRouter);
+
 app.all("/*", (req, res, next) => {
   res.status(404).send({ msg: "Route not found!" });
   next();
+});
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+  next(err);
 });
 
 app.use((err, req, res, next) => {
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const topicsRouter = require("./topicsRoutes");
+const articlesRouter = require("./articlesRoutes");
 const { getAllEndpoints } = require("../controllers/api.controller");
 
 const router = express.Router();
+router.use("/topics", topicsRouter);
+router.use("/articles", articlesRouter);
 
 router.route("/").get(getAllEndpoints);
 
