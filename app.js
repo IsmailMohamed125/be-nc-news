5acf03c186cdec0cfddaf3bd9b9e8e7f045e1ac7
ismail125 <im.1369m@gmail.com>
2024-10-15 11:04:59 +0100

    Refactored error handling

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routes/apiRoutes");
+const {
+  routeErrorHandler,
+  psqlErrorHandler,
+  customErrorHandler,
+  serverErrorHandler,
+} = require("./controllers/error.controller");
 
 app.use("/api", apiRouter);
 
-app.all("/*", (req, res, next) => {
-  res.status(404).send({ msg: "Route not found!" });
-  next();
-});
+app.all("/*", routeErrorHandler);
 
-app.use((err, req, res, next) => {
-  if (err.code === "22P02") {
-    res.status(400).send({ msg: "Bad request: Invalid category type" });
-  }
-  next(err);
-});
+app.use(psqlErrorHandler);
 
-app.use((err, req, res, next) => {
-  if (err.status && err.msg) {
-    res.status(err.status).send({ msg: err.msg });
-  }
-  next(err);
-});
+app.use(customErrorHandler);
 
-app.use((err, req, res, next) => {
-  res.status(500).send({ msg: "Server Error!" });
-});
+app.use(serverErrorHandler);
 module.exports = app;
