7fe05099717145b11d95d33c3138c289287b5791
ismail125 <im.1369m@gmail.com>
2024-10-15 16:48:18 +0100

    Updated routes, model and controller for endpoint

---
+++ controllers/users.controller.js
@@ -0,0 +1,13 @@
+const { selectUsers } = require("../models/users");
+
+const getAllUsers = (req, res, next) => {
+  selectUsers()
+    .then((users) => {
+      res.status(200).send({ users });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+module.exports = { getAllUsers };
