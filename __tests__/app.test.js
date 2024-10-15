8176e34e8334887860a6c99c6f98d6f5e74b9eb7
ismail125 <im.1369m@gmail.com>
2024-10-15 16:12:32 +0100

    Test for a valid id type but doesn't exist in DB

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -342,5 +342,13 @@ describe("Comments Endpoint", () => {
     test("DELETE:204 - Responds with no content", () => {
       return request(app).delete("/api/comments/1").expect(204);
     });
+    test("DELETE: 404 - Attempting to DELETE a resource that does not exist", () => {
+      return request(app)
+        .delete("/api/comments/999999")
+        .expect(404)
+        .then(({ body }) => {
+          expect(body.msg).toBe("Comment with id 999999 not found");
+        });
+    });
   });
 });
