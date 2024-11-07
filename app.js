38ac2317ba51e6e879ad7bc268b7e08391b428e3
ismail125 <im.1369m@gmail.com>
2024-11-07 08:44:56 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const apiRouter = require("./routes/apiRoutes");
 const {
@@ -8,6 +9,7 @@ const {
   serverErrorHandler,
 } = require("./controllers/error.controller");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
